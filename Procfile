06630ba8240913cdff90d32bbf2af519b767dcd5
flaviotech <flavio981895788@gmail.com>
2021-03-02 09:16:47 -0300

    Publicando a app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cambauba.wsgi
--- cambauba/wsgi.py
+++ cambauba/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for cambauba project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
+'''
+Favor colocar as importações em ordem alfabética para uma melhor organização
+'''
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cambauba.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
