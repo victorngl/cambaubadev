50b6db8774ec5a802c249168768ff2e1f1e70f2c
Emanuel-Bruno <emanuelbruno2018vasc@gmail.com>
2021-04-15 12:45:06 -0300

    Ajustando atributos None em informativos

--- informativos/urls.py
+++ informativos/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('balancetes/', balancetes, name="balancetes"),
     path('balancos_patrimoniais/', balancos_patrimoniais, name="balancos_patrimoniais"),
     path('documentacoes_obras/', documentacoes_obras, name="documentacoes_obras"),
-    path('atas_reunioes/', documentacoes_obras, name="atas_reunioes"),
+    path('atas_reunioes/', atas_reunioes, name="atas_reunioes"),
     path('balancetes/<int:pk>/', login_required(BalancetesDetailView.as_view(template_name="informativo_detail.html")), name="balancete_detail"),
     path('balancos_patrimoniais/<int:pk>/', login_required(BalancoPatrimonialDetailView.as_view(template_name="informativo_detail.html")), name="balanco_patrimonial_detail"),
     path('documentacoes_obras/<int:pk>/', login_required(DocumentacaoObraDetailView.as_view(template_name="informativo_detail.html")), name="documentacao_obra_detail"),
