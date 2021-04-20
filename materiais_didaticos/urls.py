25e6d309338ba84292a71b2ccb747665f0674cd0
Emanuel-Bruno <emanuelbruno2018vasc@gmail.com>
2021-04-20 15:34:24 -0300

    TRE - Adicionda single page comunicado

--- materiais_didaticos/urls.py
+++ materiais_didaticos/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('calendarios_atividades/', calendarios_atividades, name="calendarios_atividades"),
     path('comunicados/', comunicados, name="comunicados"),
     path('', materiais_didaticos, name="materiais_didaticos"),
+
     path('<int:pk>/', login_required(MateriaisDidaticosDetailView.as_view(template_name="material_didatico_detail.html")), name="material_didatico_detail"),
+
     path('calendarios_atividades/<int:pk>/', login_required(CalendariosAtividadesDetailView.as_view(template_name="material_didatico_detail.html")), name="calendario_atividade_detail"),
-    path('comunicados/<int:pk>/', login_required(ComunicadosDetailView.as_view(template_name="material_didatico_detail.html")), name="comunicado_detail"),
+
+    path('comunicados/<int:pk>/', login_required(ComunicadosDetailView.as_view(template_name="comunicado_detail.html")), name="comunicado_detail"),
 ]
