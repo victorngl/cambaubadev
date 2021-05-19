043ec5389e76bfe81e56431a208fe15c2aefc3cc
Edson de Lima Cosme Junior <edsondelimajunior@gmail.com>
2021-05-19 15:52:59 -0300

    Bug fix na validacao de acesso

--- atividades_escolares/views.py
+++ atividades_escolares/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from django.views.generic import DetailView
+from django.http import JsonResponse, HttpResponseForbidden
 from atividades_escolares.models import AtividadeEscolar
 
 @permission_required("core.pode_acessar_atividades_escolares")
