d0d3c990be549907053ea869d45bece33dcce408
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-06-10 10:46:34 -0300

    correcao HUB00084 - Erro view turma

--- atividades_escolares/models/__init__.py
+++ atividades_escolares/models/__init__.py
@@ -1,5 +1,7 @@
+from atividades_escolares.models.anexo_atividade_escolar import AnexoAtividadeEscolar
 from .atividade_escolar import AtividadeEscolar
 
 __all__ = [
-    AtividadeEscolar
+    AtividadeEscolar,
+    AnexoAtividadeEscolar
 ]
